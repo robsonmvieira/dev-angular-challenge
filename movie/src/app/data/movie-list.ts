95d008a8b6e49d7e37e11ada61db4da68a31b573
Robson Maia <robsonmvieira@gmail.com>
2021-02-02 18:24:41 -0300

    add breakpoint to table

---
+++ movie/src/app/data/movie-list.ts
@@ -0,0 +1,67 @@
+import { Movie } from "../models/movie";
+
+export const movieList: Movie[] = [
+  {
+    id: 23,
+    name: 'Os Vingadores - Guerra infinita',
+    year: 2018,
+    director: 'Joe Russo, Anthony Russo',
+    gender: 'Ação/Ficção científica',
+    description: 'Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade.',
+    cover: 'https://s2.glbimg.com/S7FJgGNQZ5TorW_xdxoMYmpIhHY=/e.glbimg.com/og/ed/f/original/2018/04/26/vingadores-guerra-infinita.jpg'
+  },
+  {
+    id: 1,
+    name: 'o lobo de Wall street',
+    year: 2013,
+    director: 'Martin Scorsese',
+    gender: 'Comédia/Drama',
+    description: 'é um ambicioso corretor da bolsa de valores que cria um verdadeiro império, enriquecendo de forma rápida, porém ilegal. Ele e seus amigos mergulham em um mundo de excessos, mas seus métodos ilícitos despertam a atenção da polícia.',
+    cover: 'www.marvel.com/infinity-war'
+  },
+  {
+    id: 5,
+    name: 'Os Últimos Cavaleiros',
+    year: 2015,
+    director: 'Kazuaki Kiriya',
+    gender: 'Ação',
+    description: 'Raiden é um guerreiro caído que deve enfrentar um corrupto e sádico governante para vingar seu mestre desonrado, Bartok.',
+    cover: 'www.marvel.com/infinity-war'
+  },
+  {
+    id: 2,
+    name: 'Regressão',
+    year: 2015,
+    director: 'Alejandro Amenábar',
+    gender: 'Drama',
+    description: 'O detetive Bruce Kenner investiga o caso da jovem Angela, que acusa seu pai de ter abusado dela. Quando o homem, de forma inesperada e sem lembrar o que aconteceu, admite sua culpa, um psicólogo intervém para buscar a verdade.',
+    cover: 'www.regressao.movie'
+  },
+  {
+    id: 2,
+    name: 'A Garota no Trem',
+    year: 2015,
+    director: 'Tate Taylor',
+    gender: 'Suspense',
+    description: 'Arrasada por seu recente divórcio, Rachel vive fantasiando sobre um casal aparentemente perfeito que mora em uma casa pela qual seu trem passa todos os dias. Certo dia, ela presencia algo estranho e começa sua própria investigação.',
+    cover: 'www.marvel.com/garota-do-trem'
+  },
+  {
+    id: 10,
+    name: 'Resident Evil 6: O Capítulo Final',
+    year: 2017,
+    director: 'Paul W. S. Anderson',
+    gender: 'Ação/Terror',
+    description: 'Sobrevivente do massacre zumbi, Alice retorna para onde o pesadelo começou, Raccoon City, em que a Umbrella Corporation reúne suas forças para um ataque final contra os remanescentes do apocalipse. Para vencer a dura batalha final e salvar a raça humana, a heroína recruta velhos e novos amigos.',
+    cover: 'http://lojasaraiva.vteximg.com.br/arquivos/ids/1721965/1003640471.jpg?v=637004921408730000'
+  },
+  {
+    id: 20,
+    name: 'Valerian e a Cidade dos Mil Planetas',
+    year: 2017,
+    director: 'Luc Besson',
+    gender: 'Ação/Ficção científica',
+    description: 'Valérian é um agente viajante do tempo e do espaço que luta ao lado da parceira Laureline, por quem é apaixonado, em defesa da Terra e seus planetas aliados, continuamente atacados por bandidos intergaláticos.',
+    cover: 'https://i.ytimg.com/vi/hG-euPafiUI/maxresdefault.jpg'
+  }
+]
---
+++ movie/src/app/models/movie.ts
@@ -0,0 +1,9 @@
+export class Movie {
+  id: number;
+  name: string;
+  year: number;
+  director: string;
+  gender: string;
+  description: string;
+  cover: string;
+}
